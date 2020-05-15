a16225f3acedfd1a0e38a6a9b77e02a4893ee4dc
bulsond <bulsond@gmail.com>
2020-05-15 16:39:14 +0300

    Init

---
+++ Task3/Phoneword/Phoneword/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Phoneword
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private string _translatedNumber;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnTranslate(object sender, EventArgs e)
+        {
+            _translatedNumber = PhonewordTranslator.ToNumber(phoneNumberText.Text);
+            if (!string.IsNullOrWhiteSpace(_translatedNumber))
+            {
+                callButton.IsEnabled = true;
+                callButton.Text = "Call " + _translatedNumber;
+            }
+            else
+            {
+                callButton.IsEnabled = false;
+                callButton.Text = "Call";
+            }
+        }
+
+        private async void OnCall(object sender, EventArgs e)
+        {
+            if (await this.DisplayAlert(
+                    "Dial a Number",
+                    "Would you like to call " + _translatedNumber + "?",
+                    "Yes",
+                    "No"))
+            {
+                var dialer = DependencyService.Get<IDialer>();
+                if (dialer != null)
+                    dialer.Dial(_translatedNumber);
+            }
+        }
+    }
+}
---
+++ Task4/Phoneword/Phoneword/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Phoneword
+{
+    public partial class App : Application
+    {
+        public static IList<string> PhoneNumbers { get; set; }
+
+        public App()
+        {
+            InitializeComponent();
+            PhoneNumbers = new List<string>();
+            MainPage = new NavigationPage(new MainPage());
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
